307a6815a7782730df3940776a3f9e98373539c8
AlamSirji <mail.alamsirji@gmail.com>
2024-02-17 11:47:55 +0530

    State Obj

--- reactapp/src/components/App.jsx
+++ reactapp/src/components/App.jsx
@@ -2,39 +2,15 @@ import React from "react";
 import { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 export default class App extends Component {
+  state = {
+    url: "https://picsum.photos/200", // use to generate random image of 200x200
+  };
   render() {
-    // Normal case for objects
-    const address = {
-      street: "Meera Bagh",
-      city: "Delhi",
-      country: "India",
-    };
-
-    console.log(address.street);
-    console.log(address.city);
-    console.log(address.country);
-
-    //Now object desctructuring goes like this way ...
-
-    // const { street, city, country } = address;
-    // console.log(street);
-    // console.log(city);
-    // console.log(country);
-
-    // OR
-
-    const { street: st, city: ci, country: cn } = address;
-    console.log(st);
-    console.log(ci);
-    console.log(cn);
-
     return (
       <React.Fragment>
-        <h1>Example of Object Desctrucuring</h1>
+        <h1>State Object</h1>
+        <img src={this.state.url} />
       </React.Fragment>
     );
-  }
-  randomNumber() {
-    return Math.floor(Math.random() * 1000);
   }
 }
