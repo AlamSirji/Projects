bd34a9e9be1eb98e78db1538646f6bbf9063d805
AlamSirji <mail.alamsirji@gmail.com>
2024-02-17 11:16:23 +0530

    Added React Fragment

--- reactapp/src/components/App.jsx
+++ reactapp/src/components/App.jsx
@@ -1,7 +1,13 @@
+import React from "react";
 import { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 export default class App extends Component {
-    render(){
-        return <h1>Hello World</h1>;
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello World</h1>
+        <h2>First React App</h2>
+      </React.Fragment>
+    );
+  }
 }
