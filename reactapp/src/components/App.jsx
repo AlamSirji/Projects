c7280a2590fc1c2dbe19a1c7d250467f6ebc2be1
AlamSirji <mail.alamsirji@gmail.com>
2024-02-17 13:54:05 +0530

    Dynamic Classes

--- reactapp/src/components/App.jsx
+++ reactapp/src/components/App.jsx
@@ -3,6 +3,7 @@ import { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 export default class App extends Component {
   state = {
+    counter: 2, // change the counter value to see the effects
     url: "https://picsum.photos/200", // use to generate random image of 200x200
   };
   styleObj = {
@@ -16,6 +17,10 @@ export default class App extends Component {
     return (
       <React.Fragment>
         <h1 className="m-2 p-2">State Object</h1>
+        <h3 className={this.getBadgeClasses()} style={this.styleObj}>
+          Counter : {this.formatCounter()}
+        </h3>
+        <hr />
         <img className="m-2 p-2" src={this.state.url} />
         <p style={{ color: "blue", padding: 10, margin: 10 }}>Inline Styling</p>
         <p style={this.styleObj}>
@@ -26,5 +31,11 @@ export default class App extends Component {
         </p>
       </React.Fragment>
     );
+  }
+  getBadgeClasses() {
+    return this.state.counter === 0 ? "text-secondary" : "text-primary";
+  }
+  formatCounter() {
+    return this.state.counter === 0 ? "zero" : this.state.counter;
   }
 }
