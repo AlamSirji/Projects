014925024db3b3381c83a9a55c4d84e328d842b6
AlamSirji <mail.alamsirji@gmail.com>
2024-02-17 13:38:47 +0530

    Inline Styling

--- reactapp/src/components/App.jsx
+++ reactapp/src/components/App.jsx
@@ -17,6 +17,7 @@ export default class App extends Component {
       <React.Fragment>
         <h1 className="m-2 p-2">State Object</h1>
         <img className="m-2 p-2" src={this.state.url} />
+        <p style={{ color: "blue", padding: 10, margin: 10 }}>Inline Styling</p>
         <p style={this.styleObj}>
           Custom styling with reactJS. Lorem ipsum dolor sit amet consectetur
           adipisicing elit. Quia error consequuntur voluptatibus labore
